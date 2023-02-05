romainl/vim-cool

commit 77aa646b63c0a2fe017bcdb033154c5fba4f947b

Fix insertion of "<7fffac03>(StopHL)" in neovim's terminal

plugin/cool.vim
@@ -75,7 +75,7 @@ function! s:StartHL()
 endfunction
 
 function! s:StopHL()
-    if !v:hlsearch || mode() isnot 'n'
+    if !v:hlsearch || mode() isnot 'n' || &buftype == 'terminal'
         return
     else
         let g:cool_is_searching = 0

commit 05ea582016aae9a6b4389a05ca1dc364e1d37a16

Fix no work on Normal mode of terminal buffers
This problem is introduced by 77aa646 because all of terminal buffers
are ignored by this plugin. Instead, add a <Plug> mapping to terminal
buffers to catch the fed mapping by Vim, not to just feed the key
sequences to terminal buffers. And this patch fixes the problem fixed by
77aa646 too.

plugin/cool.vim
@@ -75,7 +75,7 @@ function! s:StartHL()
 endfunction
 
 function! s:StopHL()
-    if !v:hlsearch || mode() isnot 'n' || &buftype == 'terminal'
+    if !v:hlsearch || mode() isnot 'n'
         return
     else
         let g:cool_is_searching = 0
@@ -101,8 +101,13 @@ function! s:PlayItCool(old, new)
         noremap <silent> <Plug>(StopHL) :<C-U>nohlsearch<cr>
         if !exists('*execute')
             noremap! <expr> <Plug>(StopHL) <SID>AuNohlsearch()
+
+            " If no "execute()", ":tnoremap" isn't probably implemented too.
         else
             noremap! <expr> <Plug>(StopHL) execute('nohlsearch')[-1]
+            if exists(':tnoremap')
+                tnoremap <expr> <Plug>(StopHL) execute('nohlsearch')[-1]
+            endif
         endif
 
         autocmd Cool CursorMoved * call <SID>StartHL()
@@ -112,6 +117,9 @@ function! s:PlayItCool(old, new)
         "   tear down coolness
         nunmap <Plug>(StopHL)
         unmap! <expr> <Plug>(StopHL)
+        if exists(':tunmap')
+            tunmap <Plug>(StopHL)
+        endif
 
         autocmd! Cool CursorMoved
         autocmd! Cool InsertEnter